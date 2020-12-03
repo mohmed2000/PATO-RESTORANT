3e14c9d8d0c4bde8cd144cfe1e54e7fb1112aeff
mohmed2000 <noza25588@gmail.com>
2020-12-03 16:16:47 +0200

    Discover-section
    #11

---
+++ src/component/Discover.jsx
@@ -0,0 +1,12 @@
+import Title from "./Title";
+export default function Discover(props) {
+  return (
+    <div className="Discover">
+      <Title
+        secTit="Discover"
+        firstTit="PATO PLACE"
+        className="black-tit text-white"
+      />
+    </div>
+  );
+}
