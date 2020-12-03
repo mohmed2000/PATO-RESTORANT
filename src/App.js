d358f5471cecd1e10c277ce47244d39c1aa6f158
mohmed2000 <noza25588@gmail.com>
2020-12-03 07:37:02 +0200

    setup-bootstarp
    #5

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import Menu from "./pages/Menu";
 import Reservation from "./pages/Reservation";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+/* libraries */
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+
 function App() {
   return (
     <div>
+      <Button>mohmed</Button>
       <Router>
         <Switch>
           <Route exact path="/">
