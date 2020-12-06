fe961cbe6bfe44c076c30228e90c19bb6f44677b
mohmed2000 <noza25588@gmail.com>
2020-12-06 17:42:59 +0200

    add_BLogSec
    update_DiscoverSec
    
    #26

--- src/component/BlogSec.jsx
+++ src/component/BlogSec.jsx
@@ -1,46 +1,48 @@
 /* component */
 import DiscoverCard from "./DiscoverCard";
+import Title from "./Title";
 /* images */
-import Card1 from "../pato images/intro/intro-01.jpg";
-import Card2 from "../pato images/intro/intro-02.jpg";
-import Card3 from "../pato images/intro/intro-04.jpg";
+import Card1 from "../pato images/blog/blog-01.jpg";
+import Card2 from "../pato images/blog/blog-02.jpg";
+import Card3 from "../pato images/blog/blog-03.jpg";
 /* libraries */
 import { Container, Row, Col } from "react-bootstrap";
 export default function Discover(props) {
   const {} = props;
   return (
-    <div>
-      <div className="Discover-cards ">
+    <div className="BlogSec">
+      <Title secTit="Latest News" firstTit="THE BLOG" className="black-tit" />
+      <div className="Discover-cards">
         <Container fluid="lg">
           <Row>
             <Col md={4}>
               <DiscoverCard
-                className="Discover-card"
+                className="Discover-card blog-card"
                 src={Card1}
-                cardTit="ROMANTIC RESTAURANT"
+                cardTit="BEST PLACES FOR WINE"
                 cardP="Phasellus lorem enim, luctus ut velit eget, con-vallis egestas eros."
-                to="/About"
-                linkCard="LEARN MORE"
+                to="/"
+                linkCard="CONTINUE READING"
               />
             </Col>
             <Col md={4}>
               <DiscoverCard
-                className="Discover-card"
+                className="Discover-card blog-card"
                 src={Card2}
-                cardTit="DELICIOUS FOOD"
+                cardTit="EGGS AND CHEESE"
                 cardP="Aliquam eget aliquam magna, quis posuere risus ac justo ipsum nibh urna"
-                to="/About"
-                linkCard="LEARN MORE"
+                to="/"
+                linkCard="CONTINUE READING"
               />
             </Col>
             <Col md={4}>
               <DiscoverCard
-                className="Discover-card"
+                className="Discover-card blog-card"
                 src={Card3}
-                cardTit="RED WINES YOU LOVE"
+                cardTit="STYLE THE WEDDING PARTY"
                 cardP="Sed ornare ligula eget tortor tempor, quis porta tellus dictum."
-                to="/About"
-                linkCard="LEARN MORE"
+                to="/Home"
+                linkCard="CONTINUE READING"
               />
             </Col>
           </Row>
---
+++ src/component/DiscoverTit.jsx
@@ -0,0 +1,16 @@
+/* component */
+import Title from "./Title";
+
+export default function DiscoverTit() {
+  return (
+    <div>
+      <div className="Discover">
+        <Title
+          secTit="Discover"
+          firstTit="PATO PLACE"
+          className="black-tit text-white"
+        />
+      </div>
+    </div>
+  );
+}
